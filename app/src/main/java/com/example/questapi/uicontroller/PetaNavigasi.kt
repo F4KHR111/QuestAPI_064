abc2eaf4ceb7b712bae513fb0e2f365764e3f04e
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-12-18 22:03:57 +0700

    Membuat file `PetaNavigasi.kt` yang berisi composable `DataSiswaApp` dan `HostNavigasi`.

---
+++ app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,19 @@
+package com.example.questapi.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+
+}

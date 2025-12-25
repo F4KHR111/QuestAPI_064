996b3015de9cd7d20e6cd3567950f8cd8b218543
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-12-25 17:12:34 +0700

    Mengisi objek `DestinasiDetail` untuk navigasi ke layar detail siswa.

--- app/src/main/java/com/example/questapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi/uicontroller/route/DestinasiDetail.kt
@@ -1,5 +1,10 @@
 package com.example.questapi.uicontroller.route
 
-object DestinasiDetail: DestinasiNavigasi {
+import com.example.questapi.R
 
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
